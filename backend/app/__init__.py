932dd650dc672af8fb5e6481728652d10aa8f588
FOTANG ATEAFACK <fotangnora@gmail.com>
2026-05-15 15:24:16 +0100

    Revert to original Docker ports and CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,12 +14,7 @@ from .middleware.error_handler import register_error_handlers
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, resources={r"/api/*": {"origins": [
-        "http://localhost:5173",
-    "https://health-symptom-checker-3fcb9.web.app",
-    "https://health-symptom-checker-3fcb9.firebaseapp.com",
-    "*"
-    ]}})
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173",}})
 
     # Logging must be first
     setup_logging()
